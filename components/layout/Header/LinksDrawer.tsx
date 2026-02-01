e700c57e96e0b60806ba579aa89fc7dd30471dcc
LukyGryc <lukygryc@seznam.cz>
2026-02-01 14:10:04 +0100

    Fixed header

--- components/layout/Header/LinksDrawer.tsx
+++ components/layout/Header/LinksDrawer.tsx
@@ -1,31 +1,31 @@
 import { AnimatedThemeToggler } from '@/components/ui/animated-theme-toggler'
 import { Button } from '@/components/ui/button'
-import { Drawer, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from '@/components/ui/drawer'
+import { Drawer, DrawerContent, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from '@/components/ui/drawer'
 import { GitHubIcon } from '@/icons/devIcons'
 import { CircleX, MenuIcon } from 'lucide-react'
 import Link from 'next/link'
-    
+
 
 const LinksDrawer = ({ links }: { links: { name: string, href: string }[] }) => {
     return (
         <Drawer direction="right">
-            <DrawerTrigger asChild className="flex items-center gap-4 h-6 block md:hidden">
+            <DrawerTrigger className="flex items-center gap-4 h-6 block md:hidden">
                 <MenuIcon className="cursor-pointer" />
             </DrawerTrigger>
             <DrawerContent>
                 <DrawerHeader>
                     <div className="flex items-center justify-between text-2xl">
                         <DrawerTitle>Menu</DrawerTitle>
-                        <DrawerTrigger asChild>
-                            <Button variant="ghost" className="cursor-pointer">
+                        <Button asChild variant="ghost" className="cursor-pointer">
+                            <DrawerTrigger>
                                 <CircleX className="size-5" />
-                            </Button>
-                        </DrawerTrigger>
+                            </DrawerTrigger>
+                        </Button>
                     </div>
                 </DrawerHeader>
-                <div className="flex flex-col gap-2 px-4 mt-4">
+                <div className="flex flex-col gap-2 px-4 mt-4 items-start">
                     {links.map(({ name, href }) => (
-                        <DrawerTrigger key={href} asChild>
+                        <DrawerTrigger key={href}>
                             <Link href={href} className="block text-xl font-bold">
                                 {name}
                             </Link>
@@ -35,14 +35,14 @@ const LinksDrawer = ({ links }: { links: { name: string, href: string }[] }) => 
                 <DrawerFooter>
                     <div className="flex items-center justify-between">
                         <AnimatedThemeToggler />
-                        <DrawerTrigger asChild>
+                        <DrawerTrigger>
                             <a
                                 href="https://github.com/LukyGryc"
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 className="block text-xl font-bold"
                             >
-                                <GitHubIcon width={24}/>
+                                <GitHubIcon width={24} />
                             </a>
                         </DrawerTrigger>
                     </div>
